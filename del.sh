c198a3bf428ac3bbdabae5a7cb9f09d1355567b7
hajiuhajiu <2705322732@qq.com>
2021-10-31 08:08:39 +0800

    Update del.sh

--- del.sh
+++ del.sh
@@ -1,5 +1,11 @@
 rm /root/jd/log -r
-rm /root/jd/scripts jd_hyj_jinbi.js
-rm /root/jd/scripts jd_temp_hyj.js
-rm /root/jd/scripts jd_NewSign.js
-rm /root/jd/scripts jd_decompression.js 
+rm /root/jd/scripts/jd_hyj_jinbi.js
+rm /root/jd/scripts/jd_temp_hyj.js
+rm /root/jd/scripts/jd_NewSign.js
+rm /root/jd/scripts/jd_decompression.js 
+rm /root/jd/scripts/main.01a74c39.js
+rm /root/jd/scripts/app.2c58486f.js
+rm /root/jd/scripts/jd_jxg.js
+rm /root/jd/scripts/app.4864fa34.js
+rm /root/jd/scripts/app.53f47a0c.js
+rm /root/jd/scripts/app.eb41fc5f.js
