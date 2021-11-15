e98a2de715be309f6695bfa1f0cf63bcfa9879a0
hajiuhajiu <2705322732@qq.com>
2021-11-15 15:44:12 +0800

    Update del.sh

--- del.sh
+++ del.sh
@@ -26,7 +26,11 @@ if [ ! -f "/root/jd/scripts/jd_djyyj.js" ];
 then  echo "文件不存在"
 else  rm -f /root/jd/scripts/jd_djyyj.js
 fi
-if [ ! -f "/root/jd/scripts/JD_extra_cookie.js " ];
+if [ ! -f "/root/jd/scripts/JD_extra_cookie.js" ];
 then  echo ""
 else  rm -f /root/jd/scripts/JD_extra_cookie.js 
+fi
+if [ ! -f "/root/jd/scripts/jd_GoldcoinToGift.js" ];
+then  echo ""
+else  rm -f /root/jd/scripts/jd_GoldcoinToGift.js 
 fi
