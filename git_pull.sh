bb9f46649be7949dce463b31f44de9e61881aaae
hajiuhajiu <2705322732@qq.com>
2021-11-15 10:45:47 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -49,7 +49,7 @@ function Git_PullShell {
  else   rm /root/jd/.git/index.lock
   fi
   if [ ! -f "/root/jd/config/config.sh" ];
- then  cp /root/jd/sample/config.sh.sample /root/jd/config/config.sh
+ then  cp /root/jd/sample/config.sh.sample /root/jd/config/config1.sh
   fi
  
   git fetch --all
