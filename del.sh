3dc610b67c62a11225d8e5da6b0f4503df0223d3
hajiuhajiu <2705322732@qq.com>
2022-01-21 16:30:15 +0800

    Update del.sh

--- del.sh
+++ del.sh
@@ -29,7 +29,7 @@ rm -f ${ShellDir}/scripts/jd_goodMorning.js
 rm -f ${ShellDir}/scripts/jd_ys.js
 rm -f ${ShellDir}/scripts/jd_city.js
 rm -f ${ShellDir}/scripts/JD_DailyBonus.js
-rm -f ${ShellDir}/scripts/jd_superBrand1.js
+rm -f ${ShellDir}/scripts/smiek2221_sign_graphics_validate.js
 
 if [ ! -f "${ShellDir}/scripts/jd_city.js" ];
 then  echo "目录文件不存在"
